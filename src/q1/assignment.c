3fe242c7064c81197a8263f7a537d383532861c3
JOHN  CUMMINS <16373731@IT.NUIGALWAY.IE>
2018-10-25 22:21:39 +0100

    Q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+	int i = 0;
+	int* j = 0;
+	long  k = 0;
+	double * q  = 0;
+	char** l = 0;
+
+	printf("Int: %d \n", sizeof(i));
+	printf("Int*: %d \n", sizeof(j));
+	printf("Long: %d \n", sizeof(k));
+	printf("Double *: %d \n", sizeof(q));
+	printf("Char**: %d \n", sizeof(l));
+
 }
+
